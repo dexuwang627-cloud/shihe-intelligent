4e1725fbdeb422c3f7b97eae7f02dd1fe03961c1
Te-Shu Wang <te-shuwang@Te-ShudeMacBook-Air.local>
2025-12-01 16:58:50 +0800

    Fix page margins and update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2025-12-01 15:47:13
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2025-12-01 16:58:30
+ * @FilePath: /shiheintelligent拷貝/vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
